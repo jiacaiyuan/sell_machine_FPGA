c4f7a1f82da7bac6c45620d7ee5083c0373e6c13
jiacaiyuan <yuan861025184@163.com>
2019-01-15 19:17:25 +0800

    add led soft

---
+++ led_soft/LERNEL_inst.v
@@ -0,0 +1,8 @@
+  //Example instantiation for system 'LERNEL'
+  LERNEL LERNEL_inst
+    (
+      .clk_0                 (clk_0),
+      .out_port_from_the_pio (out_port_from_the_pio),
+      .reset_n               (reset_n)
+    );
+
